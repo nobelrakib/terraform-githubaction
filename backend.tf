dd659c33c867c67a9da74510610172d3f20cb445
Rakib_Hossain_Novel <rakib.hossain@streamstech.com>
2025-06-20 18:58:54 +0600

    bucket name changed

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-tfstae"
+    bucket         = "terraform-bucket-tfstae--v2"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
