3ad5ca3d53588ae999a75ae464d4670d96b28350
Rakib_Hossain_Novel <rakib.hossain@streamstech.com>
2025-07-04 20:39:40 +0600

    removing extraout put

--- outputs.tf
+++ outputs.tf
@@ -14,19 +14,3 @@ output "nat_gateway_ip" {
   value       = module.vpc.nat_gateway_ip
 }
 
-# EC2 Instance Outputs for Ansible
-output "public_instance_ip" {
-  description = "Public IP address of the public EC2 instance"
-  value       = module.ec2.public_instance_ip
-}
-
-output "private_instance_ips" {
-  description = "Private IP addresses of the private EC2 instances"
-  value       = module.ec2.private_instance_ips
-}
-
-output "database_instance_ip" {
-  description = "Private IP address of the database EC2 instance"
-  value       = module.ec2.database_instance_ip
-} 
-
